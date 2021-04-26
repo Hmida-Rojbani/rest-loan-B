6265cb3546d6a87f7be57f1120294f2ab16be5f3
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-26 13:59:20 +0100

    Update LoanRestController.java

--- src/main/java/de/tekup/loan/rest/ctrl/LoanRestController.java
+++ src/main/java/de/tekup/loan/rest/ctrl/LoanRestController.java
@@ -2,7 +2,9 @@ package de.tekup.loan.rest.ctrl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,8 @@ public class LoanRestController {
 	@Autowired
 	private LoanEligebiltyService service;
 	
+	@PostMapping(path="/get-status", produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE}
+								, consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<WsResponse> getLoanStatus(@RequestBody CustomerRequest request){
 		WsResponse response = service.checkLoanEligeblity(request);
 		

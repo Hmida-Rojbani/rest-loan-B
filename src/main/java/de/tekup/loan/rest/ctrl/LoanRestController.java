7764cb0af29b03ebc55b9b2f6c20d0c27b00973c
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-04-26 13:13:27 +0100

    Create LoanRestController.java

---
+++ src/main/java/de/tekup/loan/rest/ctrl/LoanRestController.java
@@ -0,0 +1,26 @@
+package de.tekup.loan.rest.ctrl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.tekup.loan.rest.msg.CustomerRequest;
+import de.tekup.loan.rest.msg.WsResponse;
+import de.tekup.loan.rest.service.LoanEligebiltyService;
+
+@RestController
+@RequestMapping("/api")
+public class LoanRestController {
+	@Autowired
+	private LoanEligebiltyService service;
+	
+	public ResponseEntity<WsResponse> getLoanStatus(@RequestBody CustomerRequest request){
+		WsResponse response = service.checkLoanEligeblity(request);
+		
+		return new ResponseEntity<WsResponse>(response, HttpStatus.OK);
+	}
+
+}
